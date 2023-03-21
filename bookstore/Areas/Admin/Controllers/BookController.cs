9946ad54b7bacbbcce4976cd91600e7e5d9e4a21
qqphong2001 <74895683+qqphong2001@users.noreply.github.com>
2023-03-21 19:57:24 +0700

    edit book

--- bookstore/Areas/Admin/Controllers/BookController.cs
+++ bookstore/Areas/Admin/Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using bookstore.DbContext;
 using Microsoft.AspNetCore.Mvc;
 using NToastNotify;
+using System.Collections;
 
 namespace bookstore.Areas.Admin.Controllers
 {
@@ -141,6 +142,93 @@ namespace bookstore.Areas.Admin.Controllers
 
 
             return View(book);
+
+        }
+
+        [Route("editpost")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> editpost([Bind("Id,title,isbn,description,numPages,layout,publishDate,weight,translatorName,hSize,wSize,unitPrice,unitStock,discount,cover,publisher_id,author_id,genre_id")]BookModel books ,[Bind("image")] BookImagesModel bookimages,IFormFile[]? FileUploads, IFormFile imageFile)
+        {
+       
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+            var book = await _db.Books.FindAsync(books.Id);
+            if (book == null)
+            {
+                _toastNotification.AddErrorToastMessage("Không tìm thấy sản phẩm");
+                return RedirectToAction("Index");
+            }
+
+            if (imageFile != null)
+            {
+                string fileName = Guid.NewGuid().ToString();
+                string uploads = Path.Combine(wwwRootPath, @"images\products");
+                var extension = Path.GetExtension(imageFile.FileName);
+
+                await using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(fileStreams);
+                }
+                book.cover = @"\images\products\" + fileName + extension;
+            }
+            book.title = books.title;
+            book.description = books.description;
+            book.isbn = books.isbn;
+            book.discount = books.discount; 
+            book.weight = books.weight;
+            book.unitStock = books.unitStock;
+            book.unitPrice = books.unitPrice;
+            book.numPages = books.numPages; 
+            book.genre_id = books.genre_id; 
+            book.publisher_id = books.publisher_id; 
+            book.hSize = books.hSize;
+            book.author_id = books.author_id;
+            book.publishDate = books.publishDate;   
+            book.translatorName = books.translatorName;
+            book.wSize = books.wSize;
+            book.layout = books.layout;
+
+            await _db.SaveChangesAsync();
+
+
+            if (FileUploads != null)
+            {
+                var count = 0;
+                ArrayList arrList = new ArrayList();
+
+                foreach (var FileUpload in FileUploads)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    string uploads = Path.Combine(wwwRootPath, @"images\products\thumbnail");
+                    var extension = Path.GetExtension(FileUpload.FileName);
+
+                    await using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        await FileUpload.CopyToAsync(fileStream);
+                        arrList.Add(@"\images\products\thumbnail\" + fileName + extension);
+                    }
+                }
+
+                foreach (var file in arrList)
+                {
+                    count++;
+                    
+                   var  bookimage =   await _db.BookImages.FindAsync(Int32.Parse(Request.Form["book" + count]));
+                    bookimage.image = file.ToString();
+                    await _db.SaveChangesAsync();
+
+
+                }
+
+               
+
+            }
+            _toastNotification.AddSuccessToastMessage("Chỉnh sửa sản phẩm thành công");
+
+            return RedirectToAction("Index");   
+
+
 
         }
     }
