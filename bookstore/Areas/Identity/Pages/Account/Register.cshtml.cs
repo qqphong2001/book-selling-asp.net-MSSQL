48d07e652a32fa0327f73a8c96d3dd295d6bc1d4
Nghi Nguyen <ndnghi.2011@gmail.com>
2023-03-24 09:10:21 +0700

    init

--- bookstore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ bookstore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -128,7 +128,7 @@ namespace bookstore.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-                     await _userManager.AddToRoleAsync(user, Role.Role_User);
+                     await _userManager.AddToRoleAsync(user, Role.Role_Admin);
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
